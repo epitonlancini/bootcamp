73f48ca5ea04e142f1bad72dd9c179dee8fb06d8
Ricardo Shiguemi Hiramatsu <CIANDT\ricardosh@lnb028542cps.ciandt.global>
2021-02-04 17:22:15 -0300

    Insert or update rate

--- src/main/java/com/ciandt/bootcamp/controller/BreweryRateController.java
+++ src/main/java/com/ciandt/bootcamp/controller/BreweryRateController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/breweryRate")
+@RestController
 public class BreweryRateController {
 
-    @Autowired
-    private BreweryService breweryService;
+  @Autowired private BreweryService breweryService;
 
-    @PostMapping
-    public void rateBrewery(@RequestBody RateBreweryRequest request) {
-        breweryService.rateBrewery(request);
-    }
+  @PostMapping("/breweryRate")
+  public void rateBrewery(@RequestBody RateBreweryRequest request) {
+    breweryService.rateBrewery(request);
+  }
 }
