593c825dca97413e6c12731c20f7aeab1dc195a5
Ricardo Shiguemi Hiramatsu <CIANDT\ricardosh@lnb028542cps.ciandt.global>
2021-02-05 13:50:51 -0300

    Swagger docs

--- src/main/java/com/ciandt/bootcamp/controller/BreweryController.java
+++ src/main/java/com/ciandt/bootcamp/controller/BreweryController.java
@@ -2,23 +2,30 @@ package com.ciandt.bootcamp.controller;
 
 import com.ciandt.bootcamp.model.api.GetBreweryResponse;
 import com.ciandt.bootcamp.service.BreweryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/brewery")
+@Tag(name = "Brewery", description = "The Brewery API")
 public class BreweryController {
 
-    private final BreweryService breweryService;
+    @Autowired
+    private BreweryService breweryService;
 
-    public BreweryController(BreweryService breweryService) {
-        this.breweryService = breweryService;
-    }
-
-    @GetMapping("/getBreweries")
+    @Operation(summary = "Get the breweries")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Sucessfully get breweries") })
+    @GetMapping(value = "/getBreweries", produces = { "application/json" })
     public List<GetBreweryResponse> findAll() {
         return this.breweryService.findAll();
     }
