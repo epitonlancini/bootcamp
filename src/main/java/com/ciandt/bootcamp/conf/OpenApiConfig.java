5698df6098451bbfe08c1ab01fd80e9d92f2b030
Ricardo Shiguemi Hiramatsu <CIANDT\ricardosh@lnb028542cps.ciandt.global>
2021-02-05 09:45:06 -0300

    Swagger docs

---
+++ src/main/java/com/ciandt/bootcamp/conf/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package com.ciandt.bootcamp.conf;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components()).info(new Info().title("Brewery API").description(""));
+    }
+}
