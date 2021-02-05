21272967297708bb3df01622e10ff21a6fa4f770
Ricardo Shiguemi Hiramatsu <CIANDT\ricardosh@lnb028542cps.ciandt.global>
2021-02-05 14:08:03 -0300

    Check if brewery exist before save rate

--- src/main/java/com/ciandt/bootcamp/integration/service/OpenBreweryService.java
+++ src/main/java/com/ciandt/bootcamp/integration/service/OpenBreweryService.java
@@ -3,6 +3,7 @@ package com.ciandt.bootcamp.integration.service;
 import com.ciandt.bootcamp.integration.dto.OpenBreweryResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -11,14 +12,29 @@ import java.util.List;
 @Service
 public class OpenBreweryService {
 
+    // TODO: conf
+    private static String URL = "https://api.openbrewerydb.org/breweries";
+
     public List<OpenBreweryResponse> getAll() {
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "https://api.openbrewerydb.org/breweries";//TODO: conf
-        ResponseEntity<OpenBreweryResponse[]> response = restTemplate.getForEntity(url, OpenBreweryResponse[].class);
+        ResponseEntity<OpenBreweryResponse[]> response = restTemplate.getForEntity(URL, OpenBreweryResponse[].class);
 
         OpenBreweryResponse[] breweries = response.getBody();
 
         return Arrays.asList(breweries);
+    }
+
+    public OpenBreweryResponse getById(Long id) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<OpenBreweryResponse> response = restTemplate.getForEntity(URL + "/" + id,
+                    OpenBreweryResponse.class);
+
+            return response.getBody();
+        } catch (HttpStatusCodeException e) {
+            return null;
+        }
     }
 }
