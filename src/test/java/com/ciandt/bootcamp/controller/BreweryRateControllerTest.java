16a532146c59cad1e4de68175bdce8813e613b73
Evaldo Piton Lancini <epiton.lancini@gmail.com>
2021-02-05 14:00:08 -0300

    config mongo

--- src/test/java/com/ciandt/bootcamp/controller/BreweryRateControllerTest.java
+++ src/test/java/com/ciandt/bootcamp/controller/BreweryRateControllerTest.java
@@ -1,13 +1,16 @@
 package com.ciandt.bootcamp.controller;
 
 import com.ciandt.bootcamp.model.api.RateBreweryRequest;
+import com.ciandt.bootcamp.service.BreweryService;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -16,10 +19,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-//@ContextConfiguration(classes = {com.ciandt.bootcamp.service.BreweryService.class})
 @ContextConfiguration(classes = {com.ciandt.bootcamp.service.BreweryService.class})
-@WebMvcTest
-@Import(BreweryRateController.class)
+@WebMvcTest(BreweryRateController.class)
 public class BreweryRateControllerTest {
 
     @Autowired
@@ -28,10 +29,15 @@ public class BreweryRateControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    private BreweryService breweryService;
+
     @Test
     public void firstTest() throws Exception{
 
         RateBreweryRequest request = new RateBreweryRequest();
+
+        Mockito.when(breweryService.rateBrewery(request)).thenReturn(ResponseEntity.ok(null));
 
         Gson gson = new Gson();
         String json = gson.toJson(request, RateBreweryRequest.class);
