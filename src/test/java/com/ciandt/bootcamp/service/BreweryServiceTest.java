6d6b30e63a66e5b6e1f92787c29a2de0c521f38a
Evaldo Piton Lancini <epiton.lancini@gmail.com>
2021-02-05 19:39:23 -0300

    test rate

--- src/test/java/com/ciandt/bootcamp/service/BreweryServiceTest.java
+++ src/test/java/com/ciandt/bootcamp/service/BreweryServiceTest.java
@@ -1,6 +1,9 @@
 package com.ciandt.bootcamp.service;
 
 import com.ciandt.bootcamp.conf.MongoTestConfig;
+import com.ciandt.bootcamp.integration.dto.OpenBreweryResponse;
+import com.ciandt.bootcamp.integration.service.OpenBreweryService;
+import com.ciandt.bootcamp.model.api.RateBreweryRequest;
 import com.ciandt.bootcamp.model.entity.BreweryRate;
 import com.ciandt.bootcamp.repository.BreweryRateRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +14,14 @@ import org.jeasy.random.randomizers.EmailRandomizer;
 import org.jeasy.random.randomizers.range.IntegerRangeRandomizer;
 import org.jeasy.random.randomizers.range.LongRangeRandomizer;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +29,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = {MongoTestConfig.class})
 @AutoConfigureDataMongo
@@ -32,6 +41,12 @@ public class BreweryServiceTest {
 
     @Autowired
     private BreweryService breweryService;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @MockBean
+    private OpenBreweryService openBreweryService;
 
     @Test
     public void shouldSucessfullyCalculateAverage() {
@@ -64,6 +79,48 @@ public class BreweryServiceTest {
         Map<Long, Double> calculatedAverageRate = breweryService.calculateAverageRate(breweryIds);
 
         assertEquals(average, calculatedAverageRate);
+    }
+
+    @Test
+    public void shouldSuccessfullyRate() {
+
+
+        EasyRandomParameters parameters = new EasyRandomParameters();
+
+        parameters.randomize(FieldPredicates.named("breweryId")
+                .and(FieldPredicates.inClass(RateBreweryRequest.class)), new LongRangeRandomizer(1L, Long.MAX_VALUE));
+        parameters.randomize(FieldPredicates.named("rate")
+                .and(FieldPredicates.inClass(RateBreweryRequest.class)), new IntegerRangeRandomizer(1, 5));
+        parameters.randomize(FieldPredicates.named("email")
+                .and(FieldPredicates.inClass(RateBreweryRequest.class)), new EmailRandomizer());
+
+        EasyRandom easyRandom = new EasyRandom(parameters);
+
+        RateBreweryRequest request = easyRandom.nextObject(RateBreweryRequest.class);
+
+        OpenBreweryResponse openBreweryResponse = easyRandom.nextObject(OpenBreweryResponse.class);
+
+        Mockito.when(openBreweryService.getById(request.getBreweryId())).thenReturn(openBreweryResponse);
+
+        breweryService.rateBrewery(request);
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("breweryId").is(request.getBreweryId()).and("email")
+                .is(request.getEmail()));
+
+        BreweryRate breweryRate = mongoTemplate.findOne(query, BreweryRate.class);
+
+        assertNotNull(breweryRate);
+        assertEquals(breweryRate.getRate(), request.getRate());
+    }
+
+    @Test
+    public void shouldFailRateInvalidBrewery() {
+
+    }
+
+    @Test
+    public void shouldFailRateInvalidRate() {
 
     }
 
